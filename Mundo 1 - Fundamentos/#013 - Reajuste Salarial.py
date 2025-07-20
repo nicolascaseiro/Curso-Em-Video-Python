7954516646363e725a11dece38b44cc332863e6f
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-19 21:19:57 -0300

    Create #013 - Reajuste Salarial.py

---
+++ Mundo 1 - Fundamentos/#013 - Reajuste Salarial.py
@@ -0,0 +1,3 @@
+salario = float(input('Informe o salário do funcionário: R$'))
+novo_salario = salario + (salario * 0.15)
+print(f'O salário de R${salario:.2f}, com 15% de aumento foi para {novo_salario:.2f}')
