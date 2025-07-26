2c55b8de52a7080a1aeb157de83b437d5d002d7f
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-26 01:05:22 -0300

    Create #093 - Cadastro de Jogador de Futebol.py

---
+++ Mundo 3 - Estruturas Compostas/#093 - Cadastro de Jogador de Futebol.py
@@ -0,0 +1,18 @@
+jogador = dict()
+partidas = list()
+jogador['Nome'] = str(input('Nome do Jogador: '))
+total = int(input(f'Quantas partidas {jogador['Nome']} jogou? '))
+for c in range(0, total):
+    partidas.append(int(input(f'    Quantos gols na partida {c}? ')))
+jogador['Gols'] = partidas[:]
+jogador['Total'] = sum(partidas)
+print('-=' * 30)
+print(jogador)
+print('-=' * 30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-=' * 30)
+print(f'O jogador {jogador["Nome"]} jogou {len(jogador["Gols"])} partidas.')
+for i, v in enumerate(jogador['Gols']):
+    print(f'    -> Na partida {i+1}, fez {v} gols.')
+print(f'Foi um total de {jogador["Total"]} gols.')
