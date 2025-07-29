3ed1bb2bf99238c1574bc63619cc3337564307c5
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-29 15:50:31 -0300

    Create #097 - Um print especial.py

---
+++ Mundo 3 - Estruturas Compostas/#097 - Um print especial.py
@@ -0,0 +1,9 @@
+def escreva(mensagem):
+    tamanho = len(mensagem) + 4
+    print('~' * tamanho)
+    print(f'    {mensagem}')
+    print('~' * tamanho)
+
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
