c4d19ebc558bf8b2f0f5ed2fc07a6a8769d39eaf
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-30 17:16:19 -0300

    Create #104 - Validando entrada de dados em Python.py

---
+++ Mundo 3 - Estruturas Compostas/#104 - Validando entrada de dados em Python.py
@@ -0,0 +1,15 @@
+def leiaInt(mensagem):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(mensagem))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+            break
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+    return valor
+
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digita o número {n}')
