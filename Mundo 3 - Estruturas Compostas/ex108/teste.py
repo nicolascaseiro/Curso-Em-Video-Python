949fffe22cc4911b297158a673717430e819ab8f
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-31 22:36:57 -0300

    Update teste.py

--- Mundo 3 - Estruturas Compostas/ex108/teste.py
+++ Mundo 3 - Estruturas Compostas/ex108/teste.py
@@ -1,6 +1,6 @@
 from ex108 import moeda
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de R${p} é R${moeda.metade(p)}')
-print(f'O dobro de R${p} é de R${moeda.dobro(p)}')
-print(f'Aumentando 10%, temos R${moeda.aumentar(p, 10)}')
+print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
+print(f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
+print(f'Aumentando 10%, temos R${moeda.moeda(moeda.aumentar(p, 10))}')
